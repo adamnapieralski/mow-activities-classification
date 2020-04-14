35c7b6c292a8a9cb322c660b1e346eebd13f84dd
adamnapieralski <adam.napieralski@gmail.com>
2020-04-14 13:20:27 +0200

    initial commit

---
+++ rstudio-project/main_script.R
@@ -0,0 +1,2 @@
+training_data <- read.table("../../HAPT Data Set/Train/X_train.txt", header = FALSE, sep = " ")
+test_data <- read.table("../../HAPT Data Set/Test/X_test.txt", header = FALSE, sep = " ")
