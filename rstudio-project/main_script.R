7e8567019daa4ba845b17e1e37c794bcded114d3
adamnapieralski <adam.napieralski@gmail.com>
2020-05-21 21:13:09 +0200

    add data partitioning

--- rstudio-project/main_script.R
+++ rstudio-project/main_script.R
@@ -1,7 +1,14 @@
+library(caret)
+library(Boruta)
+
 remove(list=ls())
 source("preprocessing_data.R")
 source("preprocessing_class.R")
 
 all_data<-load_and_preprocess_data()
 all_classes<-load_and_preprocess_classes()
+all_data_and_classes <-cbind(all_data, all_classes)
 
+trainIndex = createDataPartition(all_data_and_classes$Class, p=.8, list=FALSE, times=1)
+train_data_and_classes <- all_data_and_classes[trainIndex,]
+validate_data_and_classes <- all_data_and_classes[-trainIndex,]
