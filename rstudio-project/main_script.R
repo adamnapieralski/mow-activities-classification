a0d13e6a56a52c9e5ce5db30127df7acd3d7f954
[michalaws2k18] <[285766@pw.edu.pl]>
2020-06-02 14:45:07 +0200

    jeszcze random rozbije tak jak bayesa

--- rstudio-project/main_script.R
+++ rstudio-project/main_script.R
@@ -4,13 +4,14 @@ library(e1071)
 library(randomForest)
 library(ROCR)
 library(pROC)
+library(rpart)
 
 
 remove(list=ls())
 source("preprocessing_data.R")
 source("preprocessing_class.R")
 
-withAllClasses=TRUE
+withAllClasses=FALSE
 all_data<-load_and_preprocess_data(withAllClasses)
 all_classes<-load_and_preprocess_classes(withAllClasses)
 
@@ -43,10 +44,61 @@ colnames(train_all)[which(names(train_all) == "train_classes")] <- "Class"
 colnames(validate_all)[which(names(validate_all) == "validate_classes")] <- "Class"
 
 #NaiveBayes
-source("naiveBayes_classification.R")
+source("naiveBayes_reduced_classification.R")
 #set.seed(1234)
-naiveBayes_classification(train_all, validate_all)
+naiveBayes_reduced_classification(train_all, validate_all)
 #RandomForest
 source("RandomForest_classification.R")
 RandomForest_classification(train_all,validate_all)
 
+source("tree_classification.R")
+tree_classification(train_all, validate_all)
+
+#Dla wszystkich klas
+withAllClasses=TRUE
+all_data_all<-load_and_preprocess_data(withAllClasses)
+all_classes_all<-load_and_preprocess_classes(withAllClasses)
+
+all_data_and_classes_all <-cbind(all_data_all, all_classes_all)
+
+# partition data 1:4
+train_index = createDataPartition(all_data_and_classes_all$Class, p=.8, list=FALSE, times=1)
+train_data_and_classes_all <- all_data_and_classes_all[train_index,]
+validate_data_and_classes_all <- all_data_and_classes_all[-train_index_all,]
+
+train_data_all <- all_data_all[train_index,]
+validate_data_all <- all_data_all[-train_index,]
+train_classes_all <- all_classes_all[train_index,]
+validate_classes_all <- all_classes_all[-train_index,]
+
+
+# long 7h run...
+# BorutaOnTraining250 <- Boruta(Class~.,data=train_data_and_classes, maxRuns=250, doTrace=2)
+
+
+# drop not important attributes
+train_data_all <- subset(train_data_all, select = c(important_attributes[,2]))
+validate_data_all<-subset(validate_data_all, select = c(important_attributes[,2]))
+#Data_binding
+train_all_all <- cbind(train_data_all,train_classes_all);
+validate_all_all<-cbind(validate_data_all,validate_classes_all)
+colnames(train_all_all)[which(names(train_all_all) == "train_classes_all")] <- "Class"
+colnames(validate_all_all)[which(names(validate_all_all) == "validate_classes_all")] <- "Class"
+
+source("naiveBayes_all_classification.R")
+#set.seed(1234)
+naiveBayes_all_classification(train_all_all, validate_all_all)
+
+plot(bayes_data_reduced[["mroc"]][[6]][["rocs"]][["4/5"]][[1]], col=colors[[1]], main="ROC dla naiwnego klasyfikatora Bayesa, klas 4 vs. 5 z klas nieprzejściowych", cex.main=0.7)
+lines(bayes_data_reduced[["mroc"]][[6]][["rocs"]][["4/5"]][[2]], col=colors[[2]])
+legend("bottomright",
+       legend = c(paste("4/5 (AUC=", round(auc(bayes_data_reduced[["mroc"]][[6]][["rocs"]][["4/5"]][[1]]), 3), ")"),
+                  paste("5/4 (AUC=", round(auc(bayes_data_reduced[["mroc"]][[6]][["rocs"]][["4/5"]][[2]]), 3), ")")),
+       col = colors[1:2],
+       pch = c(17,19), 
+       bty = "n", 
+       pt.cex = 0.8, 
+       cex = 0.7, 
+       text.col = "black", 
+       horiz = F , 
+       inset = c(0.1, 0.1))
---
+++ rstudio-project/tree_classification.R
@@ -0,0 +1,38 @@
+tree_classification <-function(train_data_and_classes, validate_data_and_classes) {
+  tree_data.classifiers <<- list()
+  tree_data.preds <<- list()
+  tree_data.preds.probs <<- list()
+  tree_data.preds.class <<- list()
+  tree_data.cm <<- list()
+  tree_data.mroc <<-list()
+  
+  train_data_and_classes_f <- train_data_and_classes
+  train_data_and_classes_f$Class <- factor(train_data_and_classes_f$Class)
+  
+  # for k-cross validation
+  n <- nrow(train_data_and_classes)
+  K <- 5
+  tail <- n%/%K
+  alea <- runif(n)
+  rn <- rank(alea)
+  bloc <- as.factor((rn - 1)%/%tail + 1)
+  
+  tree_data.err <<- list()
+  
+  for (k in 1:K) {
+    tree_data.classifiers[[k]] <<- rpart(Class~., data=train_data_and_classes_f[bloc != k,])
+    tree_data.preds.class[[k]] <<- predict(tree_data.classifiers[[k]], train_data_and_classes_f[bloc==k,], type="class")
+    tree_data.preds.probs[[k]] <<- predict(tree_data.classifiers[[k]], train_data_and_classes_f[bloc==k,])
+    tree_data.cm[[k]] <<- confusionMatrix(tree_data.preds.class[[k]], train_data_and_classes_f[bloc==k,]$Class)
+    tree_data.err[[k]] <<- tree_data.cm[[k]][["overall"]][["Accuracy"]]
+    tree_data.mroc[[k]] <<- multiclass.roc(validate_data_and_classes[bloc==k,]$Class, tree_data.preds.probs[[k]])
+  }
+  
+  #tree_data.preds.probs[[1]] <<- predict(tree_data.classifiers[[1]], subset(validate_data_and_classes, , -c(Class)))
+  #tree_data.preds.class[[1]] <<- predict(tree_data.classifiers[[1]], subset(validate_data_and_classes, , -c(Class)), type="class")
+  
+  
+  #tree_data.cm[[1]] <<- confusionMatrix(tree_data.preds.class[[1]], as.factor(validate_data_and_classes$Class))
+  #tree_data.mroc[[1]] <<- multiclass.roc(validate_data_and_classes$Class, tree_data.preds.probs[[1]])
+  
+}
